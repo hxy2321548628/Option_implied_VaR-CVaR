4b8d62db126895dc1bd3efbd01c9376c1a43267e
hxy <hxy2321548628@163.com>
2025-05-15 08:57:59 +0800

    version 1 计算期权隐含在险价值和在险价值预期

---
+++ main.py
@@ -0,0 +1,15 @@
+def main():
+    print("Hello from reseach!")
+
+b=4
+def f2(a):
+    print(a)
+    print(b)
+    b=1
+
+if __name__ == "__main__":
+    main()
+    f2(1)
+
+import html
+html.escape()
